5ebf349ec8fbd6b812ce409718b37b043ea94a9a
Alessandro Sorniotti <aso@zurich.ibm.com>
2023-05-12 19:39:17 +0200

    Implement idemix issuer with aries
    Signed-off-by: Alessandro Sorniotti <aso@zurich.ibm.com>

---
+++ bccsp/schemes/aries/issuer_test.go
@@ -0,0 +1,56 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package aries_test
+
+import (
+	"testing"
+
+	"github.com/IBM/idemix/bccsp/schemes/aries"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIssuer(t *testing.T) {
+	issuer := &aries.Issuer{}
+
+	attrs := []string{
+		"attr1",
+		"attr2",
+		"attr3",
+		"attr4",
+	}
+
+	isk, err := issuer.NewKey(attrs)
+	assert.NoError(t, err)
+	assert.NotNil(t, isk)
+
+	iskBytes, err := isk.Bytes()
+	assert.NoError(t, err)
+	assert.NotNil(t, iskBytes)
+
+	isk1, err := issuer.NewKeyFromBytes(iskBytes, attrs)
+	assert.NoError(t, err)
+	assert.NotNil(t, isk1)
+	assert.Equal(t, isk, isk1)
+
+	ipk := isk.Public()
+	assert.NotNil(t, ipk)
+
+	ipkBytes, err := ipk.Bytes()
+	assert.NoError(t, err)
+	assert.NotNil(t, ipkBytes)
+
+	ipk1, err := issuer.NewPublicKeyFromBytes(ipkBytes, attrs)
+	assert.NoError(t, err)
+	assert.NotNil(t, ipk1)
+	assert.Equal(t, ipk, ipk1)
+
+	_, err = issuer.NewKeyFromBytes([]byte("resistance is futile"), attrs)
+	assert.EqualError(t, err, "UnmarshalPrivateKey failed [invalid size of private key]")
+
+	_, err = issuer.NewPublicKeyFromBytes([]byte("resresistance is futile"), attrs)
+	assert.EqualError(t, err, "UnmarshalPublicKey failed [invalid size of public key]")
+}
