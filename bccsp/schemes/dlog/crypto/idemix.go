e40380807964c94e284829ec21f89920d308b974
Angelo De Caro <adc@zurich.ibm.com>
2026-06-30 14:15:41 +0300

    remove deprecation
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- bccsp/schemes/dlog/crypto/idemix.go
+++ bccsp/schemes/dlog/crypto/idemix.go
@@ -4,9 +4,6 @@ Copyright IBM Corp. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 
-// Deprecated: Package idemix implements the legacy CL-signature based scheme.
-// It will be removed in Phase 6 of the modernization plan. Use
-// bccsp/schemes/aries (BBS+) instead.
 package idemix
 
 import (
